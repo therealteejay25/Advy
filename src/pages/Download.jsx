06e4ac7995927c1e791089268d7da5e899818f7d
Tee Jay <therealthejay25@gmail.com>
2024-10-26 16:02:01 +0100

    commitagain

--- src/pages/Download.jsx
+++ src/pages/Download.jsx
@@ -10,7 +10,7 @@ const Download = () => {
             <h2 className='py-20 text-center text-2xl text-dark '>Your download should start very soon... <br />Thank you for using <span className='text-accent font-medium'>Advy.</span></h2>
             <div className='justify-center items-center text-center'> 
                 <Link to='/' className='border-main py-3 px-4 text-lg text-main m-3 rounded-md border-[2px]'>Back to Home</Link>
-                <a href='' className='border-[2px] border-main bg-main text-light text-lg rounded-md py-3 px-4 m-3'>Contact TMD.</a>
+                <a href='https://wa.link/bfxzhe' target='_blank' className='border-[2px] border-main bg-main text-light text-lg rounded-md py-3 px-4 m-3'>Contact TMD.</a>
             </div>
         </div>
         </div>
